4cad1fa8700b4ee023606e54e2b1985b2700f516
Johannes Winger-Lang <johannes@wingerlang.se>
2013-04-17 21:19:40 +0200

    combat-loader 0.1
    Version 0.1 - Barely just works :-)

---
+++ setrak/find_all_combats.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+#coding: cp1252
+import urllib.request, operator, re, sys, time
+from operator import itemgetter
+import parse_combat as P
+
+
+def get(url):
+	return str(urllib.request.urlopen(url).read())
+def regexresult(r):
+	if r:
+		print(r)
+	else:
+		print("No result")
+
+#parsed = str((f[f.find("pager_current"):]))
+start = 1
+stop = 20
+sleepTime = 0
+name = "Barnet Rosenbom "
+url = "http://g1.setrak.se/char/barnet_rosenbom/log?combat&page="
+
+baseurl = "http://g1.setrak.se"
+regex = "/combat/[\d]+"
+fightpage, combatlinks = [], [];
+
+for current in range(start, stop+1):
+	current_url = url + str(current)
+	page = get(current_url)
+	fightpage.append(page)
+	combatlinks.append(re.findall(regex, page))
+
+
+total_fights, times_won, times_lost, damage_dealt, damage_received = 0, 0, 0, 0, 0
+enemies = {}
+
+#print(combatlinks)
+for current_page in range(0, len(combatlinks)):
+	print("Page", current_page+1)
+	for current_combat in range(0, len(combatlinks[current_page])):
+		total_fights += 1
+		combat_url = (baseurl + combatlinks[current_page][current_combat])
+		winner = (P.battlewinner(get(combat_url)))
+		print(combat_url, winner)
+		if name in winner[0]:
+			times_won += 1
+		else:
+			if winner[0] in enemies.keys():
+				enemies[winner[0]] += 1
+			else:
+				enemies.update({winner[0]:1})
+
+			times_lost += 1
+	print("Time to sleep, %d seconds" % (sleepTime))
+	time.sleep(sleepTime)
+
+
+win_percent = 100 * (times_won / total_fights)
+lose_percent = 100 * (times_lost / total_fights)
+
+print("Total fights:", total_fights)
+print("Total won:", times_won)
+print("Total lost:", times_lost)
+print("Win/lose percent %d/%d" % (win_percent, lose_percent))
+
+enemies_sorted = (sorted(enemies.items(), key=itemgetter(1), reverse=True))
+
+for enemy in range(0, len(enemies_sorted)):
+	print(enemies_sorted[enemy])
+
+# Sort enemies by times we lost against them
---
+++ setrak/parse_combat.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#coding: cp1252
+import urllib.request
+import re, sys
+
+def battlewinner(string):
+	regex_winner = r"</p>[</div>]*<p>" + r'([a-zA-Z0-9,\\\s]+)' + "vinner striden</p>"
+	#print(regex_winner)
+	return re.findall(regex_winner, string, re.IGNORECASE)
+
+def regexresult(r):
+	if r:
+		print(r)
+	else:
+		print("No result")
+
+def removeSomeHTML(s):
+	return str(re.sub('<[^>]*>', ' ', s))
+
+def doStuff():
+	name = "Barnet Rosenbom"
+	hitstring = name + " tr\\xc3\\xa4ffar"
+
+	f = str(urllib.request.urlopen("http://g1.setrak.se/combat/386382").read())
+	parsed = " ".join(removeSomeHTML(str((f[f.find("Runda 1"):]))).split())
+
+	print(parsed)
+	print(parsed.count(hitstring))
+	print("\n")
+
+	regex = name + " tr\\\\xc3\\\\xa4ffar" + r'(.+?)' + r"\(( HP)\)"
+	regex2 = " tr\\\\xc3\\\\xa4ffar " + name + r" \((.+?)\)"
+
+
+	you_hit = re.findall(regex, parsed)
+	hit_you = re.findall(regex2, parsed)
+
+
+	regexresult(you_hit)
+	regexresult(hit_you)
+
+	print(battlewinner(f))
+
+#doStuff()
